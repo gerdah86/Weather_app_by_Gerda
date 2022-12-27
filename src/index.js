ee06df090dc96dd886fc7672eb2eaadd20049857
gerdah86 <ghincaite@yahoo.com>
2022-12-27 19:38:48 +0100

    Added forecast

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   let dayIndex = date.getDay();
   let days = [
     "Sunday",
@@ -18,7 +17,6 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
   let months = [
     "January",
     "February",
@@ -39,22 +37,17 @@ function formatDate(date) {
   return `${currentDate} ${currentMonth}, ${day} ${hours}:${minutes}`;
 }
 
-function search(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
+function search(city) {
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=26f241dba811745079f6bce715cc6450&units=${units} `;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showTemperature);
 }
-
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
 // new temp and others
-
 function showTemperature(response) {
   console.log(response);
   document.querySelector("#city").innerHTML = response.data.name;
@@ -84,24 +77,19 @@ function showTemperature(response) {
     `images/${response.data.weather[0].icon}.png`
   );
   celsiusTemperature = response.data.main.temp;
-}
 
+  getForecast(response.data.coord);
+}
 function submitting(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${city}`;
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=26f241dba811745079f6bce715cc6450&units=${units} `;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(showTemperature);
+  search(city);
 }
-
 let formSearch = document.querySelector("#search-form");
 formSearch.addEventListener("submit", submitting);
-
 // new current loc
-
 function showPosition(position) {
   let apiKey = "26f241dba811745079f6bce715cc6450";
   let lat = position.coords.latitude;
@@ -109,45 +97,51 @@ function showPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
-
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 let currentLocationButton = document.querySelector("#location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
 ////// forescast
-
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class=row>`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col">
-
-                        ${day}
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">
+                        ${formatDay(forecastDay.dt)}
                         </br>
-                        <img src="images/fewclouds.svg" width="30" />
+                        <img src="images/${
+                          forecastDay.weather[0].icon
+                        }.png" width="30" />
                         </br>
-                        <strong>+22°C</strong>
+                        <strong>${Math.round(forecastDay.temp.max)}°C</strong>
                         </br>
-                        +17°C
-
+                        ${Math.round(forecastDay.temp.min)}C
                     </div>
 `;
+    }
   });
-
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-displayForecast();
-
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 /// Fahrenheit
-
 function showFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -156,7 +150,6 @@ function showFahrenheit(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-
 function showCelsius(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
@@ -164,13 +157,9 @@ function showCelsius(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
 let celsiusTemperature = null;
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", showFahrenheit);
-
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsius);
-
 search("Malaga");
